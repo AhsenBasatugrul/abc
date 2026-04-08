970d44e3e9c0777298ecb0af0db21f027c41547e
Ahsen Basatuğrul <ahsenbasa@gmail.com>
2026-04-08 11:08:00 +0300

    ilk com

---
+++ fibonnacci.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    a, b = 0, 1
+    liste = []
+    for i in range(n):
+        liste.append(a)
+        a, b = b, a + b
+    return liste
+
+intput = int(input("Kaç tane fibonacci sayısını görmek istersiniz: "))
+print(fibonacci(intput)) 
+print(f"Fibonacci sayısı: {fibonacci(intput)}")
+print("deneme123")
